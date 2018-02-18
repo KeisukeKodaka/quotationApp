71189b7ce4d4b97525c93fa05af0a11c2ae8bd07
小高圭介 <abugarcia@u01.gate01.com>
2018-02-18 09:29:59 +0900

    Initial Commit

---
+++ QuotationApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuotationApp
+//
+//  Created by keisuke kodaka on 2018/02/18.
+//  Copyright © 2018年 Keisuke Kodaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
